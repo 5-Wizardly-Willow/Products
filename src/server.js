a451bc5ecb334ebf9efe779a31ccb46df22e2d24
Michael <kllmmc23@gmail.com>
2021-03-26 16:59:37 -0600

    add refactor server & add diskloaderio

--- src/server.js
+++ src/server.js
@@ -10,14 +10,14 @@ const cartRouter = require('./routes/cartRoutes');
 dotenv.config({ path: './config.env' });
 
 const app = express();
-const Port = 4000;
+const Port = 6000;
 //sudo docker run -d -p 4000:4000 --name products-api --env DB_URL=host.docker.internal products-api
 //docker build -f Dockerfile -t products-api .
 
 // const DB = `mongodb+srv://dbAdmin:${process.env.DB_PASSWORD}@cluster0.cffca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const DB_URL = process.env.DB_URL || `mongodb://localhost:27017/sdc`;
 const DB_OPTIONS = 'minSize=30&poolSize=100';
-
+console.log('====>>>>>>>>>>>>>>>>>', DB_URL);
 mongoose.connect(`${DB_URL}?${DB_OPTIONS}`, {
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -28,5 +28,7 @@ app.use(express.json());
 
 app.use('/products', productRouter);
 app.use('/cart', cartRouter);
+
+app.use(express.static('./diskloaderio'));
 
 app.listen(Port, () => console.log(`Port: ${Port}`));
