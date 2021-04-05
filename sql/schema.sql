1705035fffa4e072b90d6c654febc4b0f05b4e35
Michael <kllmmc23@gmail.com>
2021-04-05 06:22:13 -0600

    add sql schema improvement

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,55 +1,62 @@
+/* // http://www.plantuml.com/plantuml/png/SoWkIImgAStDuGeeoayfJIvHiD7LLGWkgSn9vN9BJImfBKfLqDMrKu3ASWLdbuBuK1LW4iXN2CZ8BybNu0fApKaiIKr1re5Lp24rBmLe9m00
+ */
 DROP DATABASE IF EXISTS products_db;
 
 CREATE DATABASE products_db;
 
 use products_db;
 
-CREATE TABLE Products = (
+CREATE TABLE Products (
     productId INT NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     slogan varchar(255),
     description varchar(255),
     category varchar(255),
-    styleId INT,
-    PRIMARY KEY (productId),
-    FOREIGN KEY (styleId) REFERENCES Styles(styleId) ON DELETE CASCADE
+    PRIMARY KEY (productId)
 )
 
-CREATE TABLE features = (
+CREATE TABLE Features (
     featureId INT NOT NULL AUTO_INCREMENT,
+    productId INT NOT NULL,
     feature varchar(255),
     value varchar(255),
-    PRIMARY KEY (featureId)
+    PRIMARY KEY (featureId),
+    FOREIGN KEY (productId) REFERENCES Products(productId) ON DELETE CASCADE,
 )
 
-CREATE TABLE Styles = (
+CREATE TABLE Styles (
     styleId INT NOT NULL AUTO_INCREMENT,
+    productId INT NOT NULL,
     name varchar(255),
-    price BIGINT,
-    salePrice BIGINT,
-    specId INT,
-    photoId INT,
+    price numeric(10,2),
+    salePrice numeric(10,2),    
     PRIMARY KEY (styleId),
-    FOREIGN KEY (photoId) REFERENCES Photos(photoId) ON DELETE CASCADE,
-    FOREIGN KEY (specId) REFERENCES Specs(specId) ON DELETE CASCADE
+    FOREIGN KEY (productId) REFERENCES Products(productId) ON DELETE CASCADE
 )   
 
-CREATE TABLE Photos = (
+CREATE TABLE Photos (
     photoId INT NOT NULL AUTO_INCREMENT,
+    styleId INT NOT NULL,
     thumbnailUrl varchar(255) NOT NULL,
     url varchar(255 ) NOT NULL,
-    PRIMARY KEY (photoId)
+    PRIMARY KEY (photoId),
+    FOREIGN KEY (styleId) REFERENCES Styles(styleId) ON DELETE CASCADE
 )  
 
-CREATE TABLE Specs = (
-    specId INT AUTO_INCREMENT,
+CREATE TABLE Skus (
+    skuId INT AUTO_INCREMENT,
+    styleId INT NOT NULL,
     sku varchar(255),
     quantity BIGINT,
     size varchar(255),
-    PRIMARY KEY (specId)
+    PRIMARY KEY (skuId),
+    FOREIGN KEY (styleId) REFERENCES Styles(styleId) ON DELETE CASCADE
 )  
 
-CREATE TABLE Related = (
-    relatedId INT NOT NULL AUTO_INCREMENT ,
-    FOREIGN KEY (productId) REFERENCES Related(relatedId) ON DELETE CASCADE,
+CREATE TABLE Related (
+    relatedId INT NOT NULL AUTO_INCREMENT,
+    fromProductId INT NOT NULL,
+    toProductId INT NOT NULL,
+    FOREIGN KEY (fromProductId) REFERENCES Products(productId) ON DELETE CASCADE,
+    FOREIGN KEY (toProductId) REFERENCES Products(productId) ON DELETE CASCADE
 )
